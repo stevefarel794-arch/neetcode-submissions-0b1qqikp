3dacb0609e3160ac4c36c80dcb18f9b6dca8fc65
stevefarel794-arch <stevefarel794@gmail.com>
2026-04-28 06:13:11 +0200

    Add: sql-indexes - submission-1

---
+++ SQL/sql-indexes/submission-1.sql
@@ -0,0 +1,11 @@
+CREATE TABLE pokemon (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    type_id INTEGER
+);
+-- Do not modify above this line. --
+CREATE INDEX name_idx ON pokemon (name);
+-- Do not modify below this line. --
+SELECT indexdef
+FROM pg_indexes
+WHERE tablename = 'pokemon';
